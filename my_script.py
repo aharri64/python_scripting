8e232b66eae9fbd7d9984586d3d207213773690c
aharri64 <haveacompleteday@gmail.com>
2021-03-17 15:18:34 -0600

    add write only script and run

--- my_script.py
+++ my_script.py
@@ -2,11 +2,25 @@
 
 def read_only():
     '''a method that only reads the fill'''
-    file1 = open('data.txt')
-    text = file1.read()
-    print(text)
-    file1.close()  # the reason for closing
+    try:
+        file1 = open('data.txt')
+        text = file1.read()
+        print(text)
+        file1.close()  # the reason for closing
+    except FileNotFoundError:
+        text = None
+        print(text)
+
+
+def write_only():
+    '''A method that writes to a file'''
+    # if file exists it will be overwritten
+    # if file doesn't exist it will be created
+    file2 = open('more_data.txt', 'w')
+    file2.write('tomatoes')
+    file2.close()
 
 
 if __name__ == '__main__':
-    read_only()
+    # read_only()
+    write_only()
